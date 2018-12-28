f75e494501b2c20f6d9997c57f95edd6e414e832
Vamadevappa, Shruthi <shruthi.vamadevappa@accenture.com>
2018-12-28 16:11:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Display from "./Display";
+import ButtonPanel from "./ButtonPanel";
+import calculate from "./calculate";
 import './App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          total: null,
+          next: null,
+          operation: null,
+        };
+      }
+
+  handleClick = buttonName => {
+    this.setState(calculate(this.state, buttonName));
+  };
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        return (
+      <div className="component-app">
+        <Display value={this.state.next || this.state.total || "0"} />
+        <ButtonPanel clickHandler={this.handleClick} />
       </div>
     );
   }
